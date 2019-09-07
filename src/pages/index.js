34eb537e213a5ab19eef895613f812cdfb65b430
zema <zlobzn@gmail.com>
2019-09-07 19:36:08 +0300

    removed old unused styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,27 +13,21 @@ export default function IndexPage({ data }) {
   return (
     <Layout>
       <SEO title="ы" />
-      <div className="homepage-columns">
-        <div className="homepage-columns__column">
-          <h2 className="homepage-columns__column-title">Заметки</h2>
-          {notes.length < 1 && <p>Все заметки куда-то потерялись… :-(</p>}
-          {notes.length > 0 && (
-            <ul className="homepage-columns__column-items zbz-links-list">
-              {notes.map(({ slug, date, title, html }) => (
-                <li
-                  key={slug}
-                  className={`zbz-links-list__item`}
-                  data-date={date}
-                >
-                  <Link className={`zbz-link`} to={slug}>
-                    {title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
+      <h2>Заметки</h2>
+      {notes.length < 1 && <p>Все заметки куда-то потерялись… :-(</p>}
+      {notes.length > 0 && (
+        <ul className="homepage-columns__column-items zbz-links-list">
+          {notes.map(({ slug, date, title, html }) => (
+            <li key={slug} className={`zbz-links-list__item`} data-date={date}>
+              <Link className={`zbz-link`} to={slug}>
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <br />
+      <br />
       <p>
         Привет. Ну раз уж вы не поленились и доскроллили до конца страницы, то
         давайте знакомиться. Меня зовут Семен. Я — программист.
